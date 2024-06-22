ee9379257a9606707e8ae487049813ec656e10c4
Vittor-Javidan <vittor.javidan.t@gmail.com>
2024-06-22 04:16:26 -0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import tmi from 'tmi.js';
-import chalk from 'chalk';
 import dotenv from 'dotenv'
+
+import usernameRandomColor from './usenameRandomColor.js';
 
 const client = new tmi.Client({
 	channels: [ dotenv.config().parsed.CHANNEL_NAME ],
@@ -28,13 +29,5 @@ client.on('message', (_, twitchResponseObj, message, self) => {
     return;
   }
 
-  const randomNumber1 = Math.random();
-  const randomNumber2 = Math.random();
-  const randomNumber3 = Math.random();
-  const randomRedValue = Math.floor(randomNumber1 * 256);
-  const randomGreenValue = Math.floor(randomNumber2 * 256);
-  const randomBlueValue = Math.floor(randomNumber3 * 256);
-  const colorizedUseName = chalk.rgb(randomRedValue, randomGreenValue, randomBlueValue)(userData.username);
-
-  console.log(`${colorizedUseName}: ${message}`);
+  console.log(`${usernameRandomColor(userData.username)}: ${message}`);
 });
